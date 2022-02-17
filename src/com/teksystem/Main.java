1996ed6346a8fedc5790cbc91f60c1349ed5c7af
Butters <k.butterfield08@gmail.com>
2022-02-17 13:31:27 -0600

    Testing Out

---
+++ src/com/teksystem/Main.java
@@ -0,0 +1,21 @@
+package com.teksystem;
+
+public class Main {
+/*
+* TODO: This is going to be awesome!
+*/
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println("Hello World!");
+        practice();
+        other other = new other();
+        other.practice();
+    }
+
+    public static void practice(){
+        System.out.println("From a funaction");
+
+    }
+}
+
+
